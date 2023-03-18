c6b30f629918047cf0562949a120ad88ab2e6c25
LightningNight1 <zoebennnettt@gmail.com>
2023-03-18 23:05:29 +0300

    Done

---
+++ oop/sem4/Apple.java
@@ -0,0 +1,9 @@
+package oop.sem4;
+
+public class Apple extends Fruit {
+
+    public Apple(double weight) {
+        super(weight);
+    }
+
+}
---
+++ oop/sem4/Orange.java
@@ -0,0 +1,9 @@
+package oop.sem4;
+
+public class Orange extends Fruit {
+
+    public Orange(double weight) {
+        super(weight);
+    }
+
+}
